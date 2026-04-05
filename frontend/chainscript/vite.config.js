ee4ee42710012252003ae4b95a9cbaed1b1fb2f2
Anmol080904 <anmolchudasma1404@gmail.com>
2026-04-05 13:02:21 +0530

    feat: Some changes

--- frontend/chainscript/vite.config.js
+++ frontend/chainscript/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Ensure the app is treated as a Single Page Application
+  appType: 'spa',
+  base: '/',
 })
